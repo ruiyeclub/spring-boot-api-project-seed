080fcae9dc38f4acb36cae8f9441275a659acd66
chenrui <185048761@qq.com>
2020-07-03 17:53:56 +0800

    runner初始化项目

---
+++ src/main/java/com/company/project/common/runner/ApplicationStartupRunner.java
@@ -0,0 +1,41 @@
+package com.company.project.common.runner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+
+/**
+ * @author Ray。
+ */
+@Component
+@Slf4j
+public class ApplicationStartupRunner implements ApplicationRunner {
+
+    @Autowired
+    private ConfigurableApplicationContext context;
+
+    @Value("${server.port}")
+    private String port;
+
+    @Value("${server.servlet.context-path:}")
+    private String contextPath;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if (context.isActive()) {
+            InetAddress address = InetAddress.getLocalHost();
+            String url = String.format("http://%s:%s", address.getHostAddress(), port);
+            if (StringUtils.isNotBlank(contextPath)) {
+                url += contextPath;
+            }
+            log.info("seed 权限系统启动完毕，地址：{}", url);
+        }
+    }
+}
