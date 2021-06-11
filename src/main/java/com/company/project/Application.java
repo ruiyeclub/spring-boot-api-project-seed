3de1248fd9e138c2409eb9bfd02bc2cecde066b8
ruiyeclub <ruiyeclub@foxmail.com>
2021-06-11 16:05:44 +0800

    modify ShiroConfig

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -2,13 +2,9 @@ package com.company.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-/**
- * @author Ray。
- */
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
